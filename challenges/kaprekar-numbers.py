8349a44cf84bae368a1587fe651c54d49cb451d9
Francis Bergin <bergin.francis@gmail.com>
2015-12-27 12:15:38 -0500

    Simplify appending of value to list in kaprekar-numbers challenge

--- challenges/kaprekar-numbers.py
+++ challenges/kaprekar-numbers.py
@@ -7,7 +7,6 @@ for i in range(p, q+1):
     d = len(str(i))
     s = str(i*i)
     if len(s) % 2 != 0: s = '0' + s
-    if int(s[:d]) + int(s[d:] if s[d:] else 0) == i:
-        a.append(i)
+    if int(s[:d]) + int(s[d:]) == i: a.append(i)
 
 print(' '.join([str(i) for i in a]) if a else 'INVALID RANGE')
