4a6e066bf157486d4a64c2d2eb85b71cd3a0537a
Francis Bergin <bergin.francis@gmail.com>
2015-12-27 12:13:38 -0500

    Simplify padding for odd number of digits in kaprekar-numbers challenge

--- challenges/kaprekar-numbers.py
+++ challenges/kaprekar-numbers.py
@@ -6,7 +6,7 @@ a = []
 for i in range(p, q+1):
     d = len(str(i))
     s = str(i*i)
-    s = s.zfill(len(s)+1 if len(s)%2!=0 else 0)
+    if len(s) % 2 != 0: s = '0' + s
     if int(s[:d]) + int(s[d:] if s[d:] else 0) == i:
         a.append(i)
 
